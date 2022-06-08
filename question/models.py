6f51d8f24785295456271ba2f50fd64a4667d0ea
Seongwon Jeon <seacom0601@naver.com>
2022-06-08 23:32:44 +0900

    generate question model and set pk on the question title

--- question/models.py
+++ question/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Question(models.Model):
+    question = models.CharField(max_length=50)
+    option = models.TextField()
+
+    def __str__(self):
+        return f'[{self.pk}]{self.question}'
 # Create your models here.
