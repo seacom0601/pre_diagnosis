7420c5b9be672d4dc4e8ba21740de4cdfd5a6586
Seongwon Jeon <seacom0601@naver.com>
2022-06-09 15:39:53 +0900

    make question app

--- question/admin.py
+++ question/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Option
 
-
-admin.site.register(Question)
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Option)
--- question/models.py
+++ question/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
+# Create your models here.
+
+
 class Question(models.Model):
     question = models.CharField(max_length=50)
-    option = models.TextField()
+
+    option1 = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'[{self.pk}]{self.question}'
-# Create your models here.
+        return f'[{self.pk}]  {self.question}'
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option = models.TextField(max_length=20)
+
+    def __str__(self):
+        return self.option
+
+
+class AnsUser(models.Model):
+    phone_num = models.CharField(max_length=13, default="000-0000-0000")
+    answer = models.CharField(default="", max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.phone_num
